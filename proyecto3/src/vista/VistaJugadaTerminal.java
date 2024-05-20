8b75c4abf67b28efb265ef606376dc4c4053c94f
Vane2005 <duran.vanessa@correounivalle.edu.co>
2024-05-20 09:46:04 -0500

    Estos son todos los cambios efectuados

--- proyecto3/src/vista/VistaJugadaTerminal.java
+++ proyecto3/src/vista/VistaJugadaTerminal.java
@@ -1,6 +1,7 @@
 package vista;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -17,69 +18,73 @@ public class VistaJugadaTerminal implements VistaJugada{
         opciones.add("PIEDRA");
         opciones.add("PAPEL");
         opciones.add("TIJERA");
-        System.out.println("Selecciona tu opcion para jugar: ");
-        for(int i=0; i<3; i++){
-            System.out.println((i+1) + ") " + opciones.get(i));
-        }
-        Scanner scanner = new Scanner(System.in);
-        byte opcion;
-        while (true) {
-            try {
-                opcion = scanner.nextByte();
-                scanner.next();
-                if(opcion<1 || opcion>3){
-                    throw new ExcepcionOpcion("OPCION NO VALIDA!!!");
-                }
-                break;
-            }catch(ExcepcionOpcion e){
-                System.out.println(e.getMessage()); 
+        opciones.add("SALIR");
+        
+        byte opcion = 0;
+        while (opcion != 4) {
+            System.out.println("Selecciona tu opcion para jugar: ");
+            for(int i=0; i<4; i++){
+                System.out.println((i+1) + ") " + opciones.get(i));
             }
-             catch (NumberFormatException e) {
-                System.out.println("DEBES DIGITAR EL NUMERO DE LA OPCION!!");
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                try {
+                    opcion = scanner.nextByte();
+                    if(opcion<1 || opcion>4){
+                        throw new ExcepcionOpcion("OPCION NO VALIDA!!!");
+                    }
+                    break;
+                }catch(InputMismatchException e){
+                    System.out.println("DEBES DIGITAR EL NUMERO DE LA OPCION!!");
+                }
+                catch (ExcepcionOpcion e) {
+                    System.out.println(e.getMessage()); 
+                }
+                scanner.nextLine();
             }
-        }
-        Random randomOpcion = new Random();
-        int opcionAleatoriaMaquina = (randomOpcion.nextInt(4)+1);
-        switch (opcion) {
-            case 1:
-                if(opcionAleatoriaMaquina == 0){
-                    System.out.println("EMPATE!!");
-                }
-                else if(opcionAleatoriaMaquina == 1){
-                    System.out.println("LA MAQUINA HA SACADO "+ (opciones.get(opcionAleatoriaMaquina)));
-                    System.out.println("TE HAN GANADO!!");
-                }
-                else{
-                    System.out.println("LA MAQUINA HA SACADO "+ (opciones.get(opcionAleatoriaMaquina)));
-                    System.out.println("HAZ GANADO!!!");
-                }
-                break;
-            case 2:
-                if(opcionAleatoriaMaquina == 1){
-                    System.out.println("EMPATE!!");
-                }
-                else if(opcionAleatoriaMaquina == 0){
-                    System.out.println("LA MAQUINA HA SACADO "+ (opciones.get(opcionAleatoriaMaquina)));
-                    System.out.println("HAZ GANADO!!!");
-                }
-                else{
-                    System.out.println("LA MAQUINA HA SACADO "+ (opciones.get(opcionAleatoriaMaquina)));
-                    System.out.println("TE HAN GANADO!!");
-                }
-                break;
-            case 3:
-                if(opcionAleatoriaMaquina == 2){
-                    System.out.println("EMPATE!!");
-                }
-                else if(opcionAleatoriaMaquina == 0){
-                    System.out.println("LA MAQUINA HA SACADO "+ (opciones.get(opcionAleatoriaMaquina)));
-                    System.out.println("TE HAN GANADO!!");
-                }
-                else{
-                    System.out.println("LA MAQUINA HA SACADO "+ (opciones.get(opcionAleatoriaMaquina)));
-                    System.out.println("HAZ GANADO!!!");
-                }
-                break;
+            Random randomOpcion = new Random();
+            int opcionAleatoriaMaquina = (randomOpcion.nextInt(3));
+            switch (opcion) {
+                case 1:
+                    if(opcionAleatoriaMaquina == 0){
+                        System.out.println("EMPATE!!");
+                    }
+                    else if(opcionAleatoriaMaquina == 1){
+                        System.out.println("LA MAQUINA HA SACADO "+ (opciones.get(opcionAleatoriaMaquina)));
+                        System.out.println("TE HAN GANADO!!");
+                    }
+                    else{
+                        System.out.println("LA MAQUINA HA SACADO "+ (opciones.get(opcionAleatoriaMaquina)));
+                        System.out.println("HAZ GANADO!!!");
+                    }
+                    break;
+                case 2:
+                    if(opcionAleatoriaMaquina == 1){
+                        System.out.println("EMPATE!!");
+                    }
+                    else if(opcionAleatoriaMaquina == 0){
+                        System.out.println("LA MAQUINA HA SACADO "+ (opciones.get(opcionAleatoriaMaquina)));
+                        System.out.println("HAZ GANADO!!!");
+                    }
+                    else{
+                        System.out.println("LA MAQUINA HA SACADO "+ (opciones.get(opcionAleatoriaMaquina)));
+                        System.out.println("TE HAN GANADO!!");
+                    }
+                    break;
+                case 3:
+                    if(opcionAleatoriaMaquina == 2){
+                        System.out.println("EMPATE!!");
+                    }
+                    else if(opcionAleatoriaMaquina == 0){
+                        System.out.println("LA MAQUINA HA SACADO "+ (opciones.get(opcionAleatoriaMaquina)));
+                        System.out.println("TE HAN GANADO!!");
+                    }
+                    else{
+                        System.out.println("LA MAQUINA HA SACADO "+ (opciones.get(opcionAleatoriaMaquina)));
+                        System.out.println("HAZ GANADO!!!");
+                    }
+                    break;
+            }
         }
     }
 
